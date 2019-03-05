9727279eff22b796f94d3cb8b27a60145b2f6f89
Antonia Uhlmann <antonia_uhlmann@web.de>
2019-03-05 14:41:43 +0100

    Test

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var app = express();
 
+var counter = 0;
+var timestamp = new Date();
+var fuck = false;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function checkupAlive() {
+  if(((new Date) - timestamp) > 60000){
+  	fuck = true;
+  }else{
+  	fuck = false;
+  }
+  setTimeout(checkupAlive, 10000);
+}
+
+setTimeout(checkupAlive, 10000);
+
+app.get('/heartbeat', function (req, res) {
+	console.log("Got a connection");
+  timestamp = new Date();
+});
+
+app.get('/checkup', function (req, res) {
+  res.send(fuck);
 });
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ' + process.env.PORT);
 });
