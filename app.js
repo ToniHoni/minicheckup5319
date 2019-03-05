bec854e0600df20d42267d0afa3894a13b457b54
ToniHoni <antonia_uhlmann@web.de>
2019-03-05 15:45:16 +0100

    demo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/heartbeat', function (req, res) {
 });
 
 app.get('/checkup', function (req, res) {
-  res.send(works);
+  res.send({"works" : works, "lastActive" : timestamp});
 });
 
 app.listen(process.env.PORT || 5000, function () {
